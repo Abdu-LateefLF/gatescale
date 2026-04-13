0c99446be3899c20cea35776b7ff2846546a7e13
Adetomiwa Lekan-Fadeyi <tomiwafadeyi@gmail.com>
2026-04-13 13:12:15 -0400

    Update cookie configs

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -7,7 +7,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const cookieOptions: CookieOptions = {
     httpOnly: true,
-    secure: true,
+    secure: isProduction ? undefined : true,
     sameSite: isProduction ? 'lax' : 'none',
 };
 
